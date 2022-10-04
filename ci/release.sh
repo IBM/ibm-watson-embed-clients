b5c0b8f819486b2877f9446c8f4a3d3f8f0ba0d8
prashantgupta24 <prashantgupta24@gmail.com>
2022-10-04 12:38:02 -0700

    👷 add release script
    Signed-off-by: prashantgupta24 <prashantgupta24@gmail.com>

---
+++ ci/release.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+# Run from the project root
+cd $(dirname ${BASH_SOURCE[0]})/..
+
+
+echo "Releasing ${VERSION}"
+
+# Dispatch to the various types of releases
+
+#this dir holds all wheel files to push later
+mkdir dist
+
+# TODO iterate through all dirs, pick only watson_* ones automatically?
+WATSON_LIBS=("watson_nlp")
+for lib in "${WATSON_LIBS[@]}"
+do
+    echo "handling $lib"
+
+    docker build -t "${lib}":"$VERSION" . \
+    --target=build \
+    --build-arg PYTHON_RELEASE_VERSION="$VERSION" \
+    --build-arg LIB_NAME="${lib}"
+
+    # copy the generated wheel file
+    docker create --name="${lib}"-tmp "${lib}":"$VERSION" && docker cp "${lib}"-tmp:/src/dist/. dist/ && docker rm "${lib}"-tmp
+
+done
